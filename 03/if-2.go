528cc11d3f4f207e2bb9d3aa7fdcd7ab11bd3c8c
kyoujuro <49559824+kyoujuro@users.noreply.github.com>
2020-03-12 21:26:21 +0900

    条件式２

---
+++ 03/if-2.go
@@ -0,0 +1,14 @@
+package main
+
+func main() {
+    test := 50
+    score := 60
+    
+    // elseで条件分岐
+    if money >= price {
+        println("合格です。")
+    } else{
+        println("不合格です。")
+    }
+    
+}
