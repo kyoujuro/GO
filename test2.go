fc0e3b8febc24bb19695c13bb37ccc901ab8c0f9
shakemaru <49559824+kyoujuro@users.noreply.github.com>
2020-12-12 17:29:30 +0900

    Update test2.go

--- test2.go
+++ test2.go
@@ -9,5 +9,15 @@ func main(){
     replacer := strings.NewReplacer("#", "o")
     fixed := replacer.Replace(broken)
     fmt.Println(fixed)
+    x := complex(2.5, 3.1)
+	fmt.Println(x)
+	y := complex(5.1, 4.9)
+	fmt.Println(y)
+	fmt.Println(x + y)
+	fmt.Println(x - y)
+	fmt.Println(x * y)
+	fmt.Println(x / y)
+	fmt.Println(real(x))
+	fmt.Println(imag(x))
 }
 
