f08675777ca5ce77d13525115023e38032191e66
shakemaru <49559824+kyoujuro@users.noreply.github.com>
2020-12-13 22:37:07 +0900

    Update test3.go

--- test3.go
+++ test3.go
@@ -1,35 +1,38 @@
 package main
 
-import(
-    "fmt"
-    "bufio"
-    "log"
-    "os"
-)
+import ("fmt")
+
+var pow = []int{1, 2, 4, 8, 16, 32, 64, 128, 256}
 
 func main(){
-    fmt.Print("Enter a grade: ")
-    reader := bufio.NewReader(os.Stdin)
-    input, err := reader.ReadString('\n')
-    log.Fatal(err)
-    fmt.Println(input)
-    var testA map[string]string = map[string]string{"Tokyo":"Tokyo", "Aichi":"Nagoya"}
-    fmt.Println(testA["Aichi"])
-    testB := make(map[string]string)
-    fmt.Println(testB)
-    testC := make(map[string]string, 20)
-    fmt.Println(testC)
-    a := Axis{10, 20}
-    fmt.Println(a.Y)
-    member := [5]int{1, 2, 3, 4, 5}
-    fmt.Println(member)
-    fmt.Println(member[1])
+	x := complex(2.5, 3.1)
+	fmt.Println(x)
+	y := complex(5.1, 4.9)
+	
+	fmt.Println(y)
+	fmt.Println(x + y)
+	fmt.Println(x - y)
+	fmt.Println(x * y)
+	fmt.Println(x / y)
+	fmt.Println(real(x))
+	fmt.Println(imag(x))
+	//fmt.Println(cmplx.Abs(x))
+	a := Axis{10, 20}
+	fmt.Println(a.Y)
+
+	member := [5]int{1, 2, 3, 4, 5}
+	fmt.Println(member)
+	fmt.Println(member[1])
+	Factorial(pow)
 }
 
-type  SerialPort_Config  struct{
-    Serial_path string,
-    Rate int,
-    Bits int,
-    Stop int,
-    Prity string,
+type Axis struct {
+	X int
+	Y int
+}
+
+func Factorial(listA []int ){
+	for i, v := range listA{
+		fmt.Printf("2**%d = %d\n", i, v)
+	}
 }
