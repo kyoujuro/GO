e04b1326ba247440b5ed98dd58866655fd1879b0
shakemaru <49559824+kyoujuro@users.noreply.github.com>
2020-12-13 22:24:54 +0900

    Update test3.go

--- test3.go
+++ test3.go
@@ -14,13 +14,16 @@ func main(){
     log.Fatal(err)
     fmt.Println(input)
     var testA map[string]string = map[string]string{"Tokyo":"Tokyo", "Aichi":"Nagoya"}
-	fmt.Println(testA["Aichi"])
-	testB := make(map[string]string)
-	fmt.Println(testB)
-	testC := make(map[string]string, 20)
-	fmt.Println(testC)
-
-    
+    fmt.Println(testA["Aichi"])
+    testB := make(map[string]string)
+    fmt.Println(testB)
+    testC := make(map[string]string, 20)
+    fmt.Println(testC)
+    a := Axis{10, 20}
+    fmt.Println(a.Y)
+    member := [5]int{1, 2, 3, 4, 5}
+    fmt.Println(member)
+    fmt.Println(member[1])
 }
 
 type  SerialPort_Config  struct{
