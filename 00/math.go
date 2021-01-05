587a1aacd78be86c9647d3db427f991c426c8a20
shakemaru <49559824+kyoujuro@users.noreply.github.com>
2021-01-05 22:33:37 +0900

    Update math.go

--- 00/math.go
+++ 00/math.go
@@ -1,13 +1,34 @@
 package main
 
+import ("fmt"
+	"time)
+	
 func main() {
 	
 	println(12 + 3)
     
-    println(3 - 6)
+    	println(3 - 6)
     
-    println(5 * 9)
+    	println(5 * 9)
 
-    println(9 % 4)
+	println(9 % 4)
+
+    
+    	t := time.Now()
+    	fmt.Println(t) 
+
+   
+   	fmt.Println(t)              
+	fmt.Println(t.Year())       
+  	fmt.Println(t.YearDay())    
+    	fmt.Println(t.Month())      
+    	fmt.Println(t.Weekday())    
+	fmt.Println(t.Day())        
+    	fmt.Println(t.Hour())       
+    	fmt.Println(t.Minute())     
+    	fmt.Println(t.Second())     
+    	fmt.Println(t.Nanosecond()) 
+    	fmt.Println(t.Zone())      
+	}
     
 }
