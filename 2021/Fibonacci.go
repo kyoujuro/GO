b60c6d33d427374c723690e1e45af6ff6bda898c
shakemaru <49559824+kyoujuro@users.noreply.github.com>
2021-01-09 23:01:37 +0900

    Create Fibonacci.go

---
+++ 2021/Fibonacci.go
@@ -0,0 +1,13 @@
+package main
+
+import ("fmt"
+		"math"
+)
+
+func main(){
+	fmt.Println(Golden(10))	
+}
+
+func Golden(n int) float64 {
+    return math.Round((math.Pow((1+math.Sqrt(5))/2, float64(n)) - math.Pow((1-math.Sqrt(5))/2, float64(n))) / math.Sqrt(5))
+}
