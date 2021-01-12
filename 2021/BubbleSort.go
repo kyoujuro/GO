207321ef9a9a4b0ec3adf1eaece838d9462c77fa
shakemaru <49559824+kyoujuro@users.noreply.github.com>
2021-01-12 21:05:05 +0900

    Update BubbleSort.go

--- 2021/BubbleSort.go
+++ 2021/BubbleSort.go
@@ -1,14 +1,27 @@
 package main
 
-import "fmt"
+import ("fmt"
+        "math/rand"
+        "time"
+)
 
-func main([]int array)[] int{
-  for i := 0; i < len(array); i++{
-    for j := i; j < i; j++{
+func BubbleSort(array []int) []int{
+  for i := 0; i < len(array) ; i++{
+    for j := i; j <len(array) ; j++{
       if array[i] < array[j]{
         array[i], array[j] = array[j], array[i]
       }
     }
   }
   return array
+}
+
+func main(){
+  rand.Seed(time.Now().UnixNano())
+	size := 100
+	array := make([]int, size, size)
+	for i := 0; i < size; i++ {
+		array[i] = rand.Intn(50)
+	}
+  fmt.Println(BubbleSort(array))
 }
