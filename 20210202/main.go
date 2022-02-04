4ed097c1c0533aa5a91783aba96249e72542bbb9
shakemaru <49559824+kyoujuro@users.noreply.github.com>
2022-02-04 22:46:40 +0900

    Update main.go

--- 20210202/main.go
+++ 20210202/main.go
@@ -6,3 +6,14 @@ import (
 	"os/exec"
 	"strings"
 )
+func main() {
+	cmd := exec.Command("ls")
+	cmd.Stdin = strings.NewReader("some input")
+	var out bytes.Buffer
+	cmd.Stdout = &out
+	err := cmd.Run()
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("in all caps: %q\n", out.String())
+}
